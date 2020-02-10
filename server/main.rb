bc0e2837bc625ad3f5a779b735ce6123aa146054
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-02-10 11:40:19 -0400

    added method stubs

--- server/main.rb
+++ server/main.rb
@@ -11,3 +11,17 @@ p Meteor.server?
 Meteor.startup do
   p "Startup"
 end
+
+Meteor.methods({
+    test: ->() do
+      p "Running test method"
+    end,
+    test2: ->(a) do
+      p "Running test2 method #{a}"
+    end
+})
+
+Meteor.call(:test)
+Meteor.call(:test2, 123) do |err, res|
+  p err, res
+end
